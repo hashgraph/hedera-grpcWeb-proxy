e3dc8d6c132cc6d8858950a6b6cbf23d84ffac21
Greg Scullard <greg@hashgraph.com>
2025-11-19 17:15:43 +0100

    Removes old expo test app
    Signed-off-by: Greg Scullard <greg@hashgraph.com>

--- TestWeb/tests/createAccount.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-import { testSetup } from './testSetup';
-
-test('createAccount', async ({ page }) => {
-    await testSetup(page);
-    await page.locator('data-testid=createAccount').click();
-    await expect(page.locator('data-testid=status')).toHaveText("SUCCESS");
-});
