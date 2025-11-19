03c065ac8729136f50f88021eea35c0f57e1f52e
Greg Scullard <greg@hashgraph.com>
2025-11-19 17:15:43 +0100

    Removes old expo test app
    Signed-off-by: Greg Scullard <greg@hashgraph.com>

--- TestWeb/tests/getBalance.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-import { testSetup } from './testSetup';
-
-test('getBalance', async ({ page }) => {
-    await testSetup(page);
-    await page.locator('data-testid=getBalance').click();
-    await expect(page.locator('data-testid=status')).not.toBeEmpty();
-});
