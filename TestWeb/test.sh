5ba16aec8a5146bbc12f04238341058af5e2b77b
Greg Scullard <greg@hashgraph.com>
2025-11-19 15:19:16 +0100

    Security fixes to testWeb
    Signed-off-by: Greg Scullard <greg@hashgraph.com>

--- TestWeb/test.sh
+++ TestWeb/test.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-# first parameter is the url of the proxy "https://grpc-web.testnet.myhbarwallet.com:443"
-# second parameter is the consensus node the proxy serves "0.0.3"
-# this parameter is either empty for all tests or a specific test (createAccount, getBalance)
-# eg ./test.sh "http://35.239.54.135:8080" "0.0.3" createAccount
-# or ./test.sh "https://grpc-web.testnet.myhbarwallet.com:443" "0.0.3"
+# first parameter is the url of the proxy "https://testnet-node02-00-grpc.hedera.com:443"
+# second parameter is the consensus node the proxy serves "0.0.5"
+# third parameter is your network account id "0.0.1234"
+# fourth parameter is your network account private key (ED25519)
+# the fifth parameter is either empty for all tests or a specific test (createAccount, getBalance)
+# eg ./test.sh "https://testnet-node02-00-grpc.hedera.com:443" "0.0.5" "0.0.1234" "ED25519 private key" createAccount
+# or ./test.sh "https://testnet-node02-00-grpc.hedera.com:443"  "0.0.5" "0.0.1234" "ED25519 private key"
 
 export PROXY_URL=$1
 export NODE_ACCOUNT_ID=$2
-export OPERATOR_ID="0.0.11093"
-export PRIVATE_KEY="302e020100300506032b657004220420f28afe2490e6622feb57a59a5e78a84db8f32bf54250a29e0fffd013c87a423c"
-export WEBAPP_URL="http://localhost:19006/"
+export OPERATOR_ID=$3
+export PRIVATE_KEY=$4
+export WEBAPP_URL="http://localhost:8081/"
 
 #export PWDEBUG=1
 
-npx playwright test $3
+npx playwright test $5
 
 if [ $? -eq 0 ]
 then
